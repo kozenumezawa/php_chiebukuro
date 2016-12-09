f84111d74d3ef6b52ac8d25b78d3e9fba53c6e80
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-12-09 15:30:08 +0900

    Add: to write a result

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 
 import lxml.html
 import requests
+import codecs
 
 def getDOM(target_url):
     target_html = requests.get(target_url).text
@@ -11,6 +12,9 @@ def getDOM(target_url):
 f = open('url.txt')
 url = f.read()
 f.close()
+
+f_question = codecs.open('love_question.txt', 'w', 'utf-8')
+f_answer = codecs.open('love_answer.txt', 'w', 'utf-8')
 
 top_dom = getDOM(url)
 qalst = top_dom.get_element_by_id('qalst')
@@ -27,10 +31,13 @@ for li_tag in qalst:
     div_question = detail_dom.xpath('//div[@class="ptsQes"]')
     p_question_1 = div_question[0][0].text_content()
     p_question_2 = div_question[0][1].text_content()
-    p_question = p_question_1.strip() + p_question_2.strip()
-    # print(p_question)
+    question = p_question_1.strip() + p_question_2.strip()
+    f_question.write(question)
 
     # get a best answer
     best_answer = div_question[0].text_content()
     best_answer = div_question[1][0].text_content()
     best_answer = best_answer.strip()
+    f_answer.write(best_answer)
+f_question.close()
+f_answer.close()
